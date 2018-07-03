de3128adaca0475480fc7c2207f020f545439b40
林福廷 <nana14352000@gmail.com>
2018-07-04 02:58:57 +0800

    final

--- HW2.py
+++ HW2.py
@@ -1,7 +1,25 @@
-import numpy as np 
 import matplotlib.pyplot as plt 
 from sklearn import datasets
 from sklearn import svm
 from sklearn import metrics
+
 database = datasets.load_digits()
-print(len(database.images))
+print("databasesize:", len(database.images))
+
+clf = svm.SVC(gamma=0.001)
+x,y = database.data[:1000], database.target[:1000]
+clf.fit(x, y)
+
+expected = database.target[1000:]
+predicted = clf.predict(database.data[1000:])
+
+img_predict = list(zip(database.images[1000:], predicted))
+for i, (image, prediction) in enumerate(img_predict[:5]):
+    plt.subplot(1, 5, i+1)
+    plt.imshow(image, cmap=plt.cm.gray_r, interpolation='none')
+    plt.title('Prediction:%i' % prediction)
+
+plt.show()
+
+print("classifier report\n%s\n" % (metrics.classification_report(expected, predicted)))
+print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
